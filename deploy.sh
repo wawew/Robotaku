cd0971769820af2123f5159342ba9a1ad3abbd49
Wildan Firdaus <57925194+wiflash@users.noreply.github.com>
2020-02-26 12:27:38 +0700

    forced kill app in certain port

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,5 @@ docker stop robotaku-backend
 docker container prune -f
 docker rmi wiflash/robotaku-container:be-latest
 docker image prune -f
+sudo lsof -t -i tcp:5000 | sudo xargs kill -9
 docker run -d --name robotaku-backend -p 5000:5000 wiflash/robotaku-container:be-latest
